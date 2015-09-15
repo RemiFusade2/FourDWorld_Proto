e5a60d5e8b49246f734ca14bce15897a29aa5853
Remi <remi.fusade@gmail.com>
2015-09-15 08:52:04 +0200

    Big Update + Classic Skybox + Terrain Assets + sky5x + Custom animations
    Signed-off-by: Remi <remi.fusade@gmail.com>

---
+++ Assets/Scripts/PlacardDoorBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlacardDoorBehaviour : MonoBehaviour {
+
+
+	void OnMouseDown()
+	{
+		this.GetComponent<Animator> ().SetBool ("Open", !this.GetComponent<Animator> ().GetBool ("Open"));
+	}
+}
